f0483514f523b78d05ec24e400dc62af069b421f
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2024-09-08 18:48:29 -0500

    Reto 00 completado

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/C4rlo5Riv3ra.java
@@ -0,0 +1,34 @@
+// Sitio web oficial de Java
+// https://www.java.com/
+
+// comentario
+
+/* 
+sintaxis d
+comentario en
+varias lineas 
+*/
+
+public class C4rlo5Riv3ra {
+    public static void main(String[] args) {
+
+        // declaracion de variables
+        String name = "Carlos Rivera";
+        // declaracion de constantes
+        final double pi = 3.14;
+        // declaracion de variables con tipos de datos primitivos
+        byte numByte = 127;
+        short numShort = 32767;
+        int numInt = 2147483647;
+        long numLong = 9223372036854775807L;
+        float numFloat = 3.14f;
+        double numDouble = 3.14;
+        char numChar = 'a';
+        boolean numBoolean = true;
+        String lenguaje = "Java";
+
+        // imprimir a traves de la consola
+        System.out.println("Hola, " + lenguaje);
+
+    }
+}
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/C4rlo5Riv3ra.js
@@ -0,0 +1,33 @@
+// Sitio web oficial de Javascript
+// https://www.javascript.com/
+
+// comentario
+
+/* sintaxis d
+comentario en
+varias lineas
+*/
+
+let n = 10;
+let nombre = "Carlos Rivera";
+const pi = 3.14; //valor constante
+
+
+//Datos Primitivos en Javascript
+//1. String
+let lenguaje = "Javascript"
+//2. Number
+let a = 20;
+//3. Boolean
+let b = true
+let c = false
+//4. Undefined
+let d;
+//5. Null
+let e = null
+//6. Symbol
+let f = Symbol()
+
+// Imprimir a traves de la consola
+
+console.log("Hola, " + lenguaje);
